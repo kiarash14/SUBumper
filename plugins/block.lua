ba6804dac1ef3f8f1151747e16a3a90d30f0f666
tgcloner <aminaleahmad10@gmail.com>
2016-01-29 01:06:57 +0330

    Update block.lua

--- plugins/block.lua
+++ plugins/block.lua
@@ -5,7 +5,7 @@ local function run(msg, matches)
       end
 	local user_id = "user#id"..matches[2]
 	block_user(user_id, ok_cb, false)
-	return "```🔒*User Has Been Blocked*🔒```"
+	return "🔒*User Has Been Blocked*🔒"
     end
     if matches[1] == "unblock" then
 	if not is_sudo(msg) then
@@ -19,8 +19,10 @@ end
 
 return {
   patterns = {
-    "^[!/](block) (.*)$",
-    "^[!/](unblock) (.*)$"
+    "^[!/.](block) (.*)$",
+    "^[!/.](unblock) (.*)$",
+        "^(block) (.*)$",
+    	"^(unblock) (.*)$",
   },
   run = run
 }
