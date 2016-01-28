e525df32db58219ec51aa5968014445ca1420550
tgcloner <aminaleahmad10@gmail.com>
2016-01-29 00:53:56 +0330

    Update fosh.lua

--- plugins/fosh.lua
+++ plugins/fosh.lua
@@ -1,15 +1,15 @@
 do
 
 function run(msg, matches)
-	return "کیر امین و علیرضا تو کس ننت" .. matches[1]
+	return "˜?Ñ Ó?ÇÑå æ Îá?Ì ÝÇÑÓ Êæ ˜Ó ääå ÌäÏÊ" .. matches[1]
 end
 end
 return {
   description = "Says Hello to Someone", 
   usage = "Say Hello to (name)",
   patterns = {
-    "^be (.*) fosh bede",
-	"^be (.*) salam kon",
+    "^fosh (.*) ",
+	"^Cuss (.*) ",
   }, 
   run = run 
 }
