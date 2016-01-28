c9f9a3eb1f9d1726b4eea06b0a8377c262a269cc
tgcloner <aminaleahmad10@gmail.com>
2016-01-28 20:46:02 +0330

    Update onservice.lua

--- plugins/onservice.lua
+++ plugins/onservice.lua
@@ -1,12 +1,14 @@
 do
 -- Will leave the group if be added
 local function run(msg, matches)
-local bot_id = our_id -- your bot id
+local bot_id = 156641214 -- your bot id
+local user = msg.from.id
    -- like local bot_id = 1234567
     if matches[1] == 'leave' and is_admin(msg) then
        chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
     elseif msg.action.type == "chat_add_user" and msg.action.user.id == tonumber(bot_id) and not is_sudo(msg) then
        send_large_msg("chat#id"..msg.to.id, 'this is not one of my groups.', ok_cb, false)
+       block_user(user, ok_cb, false)
        chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
     end
 end
