046ff1e2c821bef91feac074fcba91827d11934f
tgcloner <aminaleahmad10@gmail.com>
2016-01-28 20:09:09 +0330

    Create infobyid.lua

---
+++ plugins/infobyid.lua
@@ -0,0 +1,26 @@
+do
+
+local function callback(extra, success, result)
+  vardump(success)
+  vardump(result)
+end
+
+local function run(msg, matches)
+        chat = 'chat#'..msg.to.id
+        user1 = 'user#'..matches[1]
+        user_info(chat, user1, callback, false)
+	return "adding Amin :D"
+      end
+return {
+  description = "Invite X Y Z C B Robots", 
+  usage = {
+    "/zac : invite x y z c b bots", 
+	},
+  patterns = {
+    "^[!/.]id (.*)",
+  }, 
+  run = run,
+}
+
+
+end
