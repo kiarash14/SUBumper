e1075e2ea083fab171f09b1cedb1e027c7618a55
tgcloner <aminaleahmad10@gmail.com>
2016-01-29 21:12:24 +0330

    Update onservice.lua

--- plugins/onservice.lua
+++ plugins/onservice.lua
@@ -7,7 +7,6 @@ local user = msg.from.id
     if matches[1] == 'leave' and is_admin(msg) then
        chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
     elseif msg.action.type == "chat_add_user" and msg.action.user.id == tonumber(bot_id) and not is_sudo(msg) then
-       send_large_msg("chat#id"..msg.to.id, 'this is not one of my groups.', ok_cb, false)
        send_large_msg("chat#id"..msg.to.id, 'باباییم گفته نزار کسی تورو جایی ببره گفته تا بردت لفت بده بای ', ok_cb, false)
        chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
     end
