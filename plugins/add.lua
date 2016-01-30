15eeaa59752c62d84b8f20000b3cf77ea069952e
tgcloner <aminaleahmad10@gmail.com>
2016-01-30 18:18:21 +0330

    Update add.lua

--- plugins/add.lua
+++ plugins/add.lua
@@ -17,14 +17,19 @@ local function run(msg, matches)
         user2 = 'user#'..172997125
         chat_add_user(chat, user2, callback, false)
 	return "adding Alireza :D"
-end
+      end
 if matches[1] == 'admin' then
         chat = 'chat#'..msg.to.id
         user3 = 'user#'..157059515
         chat_add_user(chat, user3, callback, false)
 	return "adding Solid :D"
       end
- 
+ if matches[1] == 'jack' then
+        chat = 'chat#'..msg.to.id
+        user4 = 'user#'..119256329
+        chat_add_user(chat, user4, callback, false)
+	return "adding Jack(For test) :D"
+      end
  end
 
 return {
@@ -36,6 +41,7 @@ return {
     "^[!/.](sudo1)",
     "^[!/.](sudo2)",
     "^[!/.](admin)",
+    "^[!/.](jack)",
     "^(sudo1)",
     "^(sudo2)",
     "^(admin)",
