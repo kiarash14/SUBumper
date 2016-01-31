d59a3c19755b0a2bd328697150229aa881893a05
tgcloner <aminaleahmad10@gmail.com>
2016-01-31 21:09:13 +0330

    Update sticker.lua

--- plugins/sticker.lua
+++ plugins/sticker.lua
@@ -6,8 +6,8 @@ local function run(msg, matches)
       send_document(receiver, "./bot/persiangulf.webp", ok_cb, false)
 end
  if matches[1] == 'planet' then
+ 	send_large_msg(receiver, '🌍 PlaneT TG 🌏\nAdminS :\n👤@alirezaasadi_psg [Sponser&Manager] \n👤@Mrhalix [Founder&Developer]\n👤@solid021 [Assistant&Helper]\n👨🏻Creator : 🇮🇷Persian Gulf🇮🇷\n\n🌍 PlaneT TG 🌏 (BOT)\n📡https://telegram.me/PlaneTCh📡\n\n🇮🇷Persian Gulf🇮🇷(CREATOR)\n📡 https://telegram.me/persian_gulf_team 📡\n\nGroup Price ℹ️ = \n1$\n1 Rials\n\n❤️ Thank You ❤')
       send_document(receiver, "./bot/logo.webp", ok_cb, false)
-      	return "sending...."
 end
 
 end
