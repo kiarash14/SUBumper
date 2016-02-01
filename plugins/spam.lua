e43e758c45d1dfda2e2046f4a027b8eed9b56451
tgcloner <aminaleahmad10@gmail.com>
2016-02-01 18:16:18 +0330

    Update spam.lua

--- plugins/spam.lua
+++ plugins/spam.lua
@@ -4,7 +4,7 @@ function run(msg, matches)
 
   local tex = matches[1]
   local sps = matches[2]
-  local sp = ':|'
+  local sp = '\n:|'
   for i=1, tex, 1 do 
       sp = '\n'..sps..'\n'..sp
   end 
