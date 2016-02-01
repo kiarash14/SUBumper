80d9bd1a245c3390dc92216a177ca182e5ba3d59
tgcloner <aminaleahmad10@gmail.com>
2016-02-01 20:49:03 +0330

    Create danestani.lua

---
+++ plugins/danestani.lua
@@ -0,0 +1,13 @@
+local database = 'http://mpfan.ir/'
+local function run(msg)
+local res = http.request(database.."danestani.db")
+local danestani = res:split(",")
+return danestani[math.random(#danestani)]
+end
+
+return {
+description = "Knowing Book",
+usage = "!danestani : send knowing texts",
+patterns = {"^[/!]danestani$"},
+run = run
+}
