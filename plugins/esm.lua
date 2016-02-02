fded8b95f1bc6e573fe4456469709e4412300072
tgcloner <aminaleahmad10@gmail.com>
2016-02-02 17:25:25 +0330

    Update esm.lua

--- plugins/esm.lua
+++ plugins/esm.lua
@@ -1,20 +1,12 @@
 do
 local function run(msg)
-if not is_sudo(msg) then
 local database = 'http://mpfan.ir/'
 local function run(msg)
 local res = http.request(database.."esm.db")
 local esm = res:split(",")
 return esm[math.random(#esm)]
 end
-end
-local database = 'http://mpfan.ir/'
-local function run(msg)
-local res2 = http.request(database.."esmsudo.db")
-local esmsudo = res2:split(",")
-return esmsudo[math.random(#esmsudo)]
-end
-end
+
 
 return {
 description = "Jomlak Sender",
