7218faf6325889cd5fb7922fcdeda8a516cca646
SoLiD <solid.in89@yahoo.com>
2016-02-15 19:07:02 +0330

    Update add.lua

--- plugins/add.lua
+++ plugins/add.lua
@@ -6,23 +6,23 @@ local function callback(extra, success, result)
 end
 
 local function run(msg, matches)
- if matches[1] == 'sudo1' then
+ if matches[1] == 'solid' then
         chat = 'chat#'..msg.to.id
-        user1 = 'user#'..140529465
+        user1 = 'user#'..157059515
         chat_add_user(chat, user1, callback, false)
-	return "adding Amin :D"
+	return "Solid Is Coming :/"
       end
-  if matches[1] == 'sudo2' then
+  if matches[1] == 'parsa' then
         chat = 'chat#'..msg.to.id
-        user2 = 'user#'..172997125
+        user2 = 'user#'..162108576
         chat_add_user(chat, user2, callback, false)
-	return "adding Alireza :D"
+	return "Parsa Is Coming :D"
       end
-if matches[1] == 'admin' then
+if matches[1] == 'mrhalix' then
         chat = 'chat#'..msg.to.id
-        user3 = 'user#'..157059515
+        user3 = 'user#'..140529465
         chat_add_user(chat, user3, callback, false)
-	return "adding Solid :D"
+	return "MrHalix Is Coming :D"
       end
  if matches[1] == 'jack' then
         chat = 'chat#'..msg.to.id
@@ -38,13 +38,13 @@ return {
     "/zac : invite x y z c b bots", 
 	},
   patterns = {
-    "^[!/.](sudo1)",
-    "^[!/.](sudo2)",
-    "^[!/.](admin)",
+    "^[!/.](solid)",
+    "^[!/.](parsa)",
+    "^[!/.](mrhalix)",
     "^[!/.](jack)",
-    "^(sudo1)",
-    "^(sudo2)",
-    "^(admin)",
+    "^(solid)",
+    "^(parsa)",
+    "^(mrhalix)",
   }, 
   run = run,
 }
