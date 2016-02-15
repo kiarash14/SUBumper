ee549ede9ce56e128e22dec2f1524b5f5675ade7
SoLiD <solid.in89@yahoo.com>
2016-02-15 19:15:37 +0330

    Update about.lua

--- plugins/about.lua
+++ plugins/about.lua
@@ -1,8 +1,7 @@
 local function run(msg)
-  local about = "🌍 PlaneT TG 🌏\nAdminS :\n👤@alirezaasadi_psg [Sponser&Manager] \n👤@Mrhalix [Founder&Developer]\n👤@solid021 [Assistant&Helper]\n👨🏻Creator : 🇮🇷Persian Gulf🇮🇷\n\n🌍 PlaneT TG 🌏 (BOT)\n📡https://telegram.me/PlaneTCh📡\n\n🇮🇷Persian Gulf🇮🇷(CREATOR)\n📡 https://telegram.me/persian_gulf_team 📡\n\nGroup Price ℹ️ = \n1$\n1 Rials\n\n❤️ Thank You ❤"
+  local about = "🌍 PlaneT TG 🌏\nAdminS :\n👤@parsa_psg [Sponser&Manager] \n👤@Mrhalix [Founder&Developer]\n👤@solid021 [Assistant&Helper]\n👨🏻Creator : 🇮🇷Persian Gulf🇮🇷\n\n🌍 TeLe Gulf 🌏 (BOT)\n📡https://telegram.me/TeleGulf_Ch📡\n\n🇮🇷Persian Gulf🇮🇷(CREATOR)\n📡 https://telegram.me/persiangulf_team 📡\n\nGroup Price ℹ️ = \n1$\n1 Rials\n\n❤️ Thank You ❤"
         return about
     end
 return {patterns = {
    "^[!/.](about us)",
-   "^(درباره)$",
     }, run = run}
