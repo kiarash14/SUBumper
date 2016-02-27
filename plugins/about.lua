d322ff0fd4f9837b4c8bc021863729266ecc3b4b
kiarash14 <omidbakhsh22@gmail.com>
2016-02-27 10:06:47 +0330

    Delete about.lua

--- plugins/about.lua
+++
@@ -1,7 +0,0 @@
-local function run(msg)
-  local about = "🌍 PlaneT TG 🌏\nAdminS :\n👤@parsa_psg [Sponser&Manager] \n👤@Mrhalix [Founder&Developer]\n👤@solid021 [Assistant&Helper]\n👨🏻Creator : 🇮🇷Persian Gulf🇮🇷\n\n🌍 TeLe Gulf 🌏 (BOT)\n📡https://telegram.me/TeleGulf_Ch📡\n\n🇮🇷Persian Gulf🇮🇷(CREATOR)\n📡 https://telegram.me/persiangulf_team 📡\n\nGroup Price ℹ️ = \n1$\n1 Rials\n\n❤️ Thank You ❤"
-        return about
-    end
-return {patterns = {
-   "^[!/.](about us)",
-    }, run = run}
