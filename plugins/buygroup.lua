ce3b5ce2e36fbd8dc5c29240a9663a7e782280a7
kiarash14 <omidbakhsh22@gmail.com>
2016-02-27 09:27:17 +0330

    Update buygroup.lua

--- plugins/buygroup.lua
+++ plugins/buygroup.lua
@@ -1,7 +1,8 @@
 local function run(msg)
-    local buy = "For buy a group go 1 of this ids : \n @Mrhalix \n\n @alirezaasadi_psg\n\n Those ids Reported pls send 10 msg :D\n➕➕➕➕➕➕➕\nبرای خرید گروه به آیدی های زیر بروید!!\n @Mrhalix\n\n@alirezaasadi_psg\nدو آیدی ریپورت هستند"
+    local buy = "For buy a group go 1 of this ids : \n @xXx_reza_xXx \n\n @kiarash_gh14\n\n Those ids Reported pls send 10 msg :D\n➕➕➕➕➕➕➕\nبرای خرید گروه به آیدی های زیر بروید!!\n @Mrhalix\n\n@alirezaasadi_psg\nدو آیدی ریپورت هستند"
 return buy
 end
 return {patterns = {
-   "[!/.]buy",
+   "[!/#i]buy",
+   "buy",
     }, run = run}
